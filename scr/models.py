3e7180929a0c26ee7ca7fbc69ecebdbbcf1ad696
bourahima-coulibaly <153512895+bourahima-coulibaly@users.noreply.github.com>
2024-07-19 00:33:10 +0200

    sec

--- scr/models.py
+++ scr/models.py
@@ -7,7 +7,7 @@ from scr.utils import ModelError
 from abc import ABC, abstractmethod
 
 
-HUGGINGFACE_HUB_API_TOKEN = st.secrets["HUGGINGFACE_HUB_API_TOKEN"]
+HUGGINGFACE_HUB_API_TOKEN = st.secrets.get("HUGGINGFACE_HUB_API_TOKEN", None)
 
 
 class ResumeAIStrategy(ABC):
